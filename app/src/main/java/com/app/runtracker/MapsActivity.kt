31bfbaa35a904905667d3a43f084e0f0c84cd85b
raheez66 <raheez66@gmail.com>
2022-03-02 17:11:06 +0400

    evening commit,worked on location tracking

--- app/src/main/java/com/app/runtracker/MapsActivity.kt
+++ app/src/main/java/com/app/runtracker/MapsActivity.kt
@@ -1,5 +1,6 @@
 package com.app.runtracker
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.Color
@@ -8,8 +9,9 @@ import android.location.LocationListener
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
-import android.widget.Toast
+import android.view.View
 import androidx.core.app.ActivityCompat
 import androidx.core.location.LocationManagerCompat.isLocationEnabled
 
@@ -21,9 +23,10 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.app.runtracker.databinding.ActivityMapsBinding
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.PolylineOptions
-import java.util.jar.Manifest
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
 
@@ -39,6 +42,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
 
     private lateinit var mFusedLocationProvider: FusedLocationProviderClient
     private lateinit var mLocationManager: LocationManager
+    private var mLocationRequest = com.google.android.gms.location.LocationRequest()
     //endregion
 
 
@@ -55,22 +59,16 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
         mapFragment.getMapAsync(this)
 
         initListener()
-        val mPoint1 = LatLng(21.00, 51.00)
-        val mPoint2 = LatLng(31.00, 61.00)
-        val mPoint3 = LatLng(41.00, 71.00)
-        val mPoint4 = LatLng(51.00, 81.00)
-
         mPointsList = ArrayList<LatLng>()
-        mPointsList?.add(mPoint1)
-        mPointsList?.add(mPoint2)
-        mPointsList?.add(mPoint3)
-        mPointsList?.add(mPoint4)
-
-
         mFusedLocationProvider = LocationServices.getFusedLocationProviderClient(this)
         mLocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        getLastLocation()
 
+        if (checkIfPermissionGranted()) {
+
+        } else {
+            Log.d("permission_denied", "please_grant_the_permission")
+            requsetPermission()
+        }
     }
 
     override fun onResume() {
@@ -83,38 +81,82 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
 
     override fun onDestroy() {
         super.onDestroy()
+        mFusedLocationProvider?.removeLocationUpdates(mLocationCallback)
     }
 
     //endregion
 
 
     //region methods
-    private fun initListener() {
-        binding?.startButton?.setOnClickListener {
-            mIsCollectLocation = true
-            Toast.makeText(this, "started", Toast.LENGTH_SHORT).show()
-        }
-        binding?.stopButton?.setOnClickListener {
-            mIsCollectLocation = false
-            mMap.clear()
-            mMap.addPolyline(
-                PolylineOptions().addAll(mPointsList!!).width(15f).color(Color.BLUE).geodesic(true)
-            )
-            Toast.makeText(this, "stopped", Toast.LENGTH_SHORT).show()
+
+    val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            super.onLocationResult(locationResult)
+            for (location in locationResult.locations) {
+                if (mIsCollectLocation) {
+                    var mLatLng: LatLng
+                    mLatLng = LatLng(location.latitude, location.longitude)
+                    Log.d("new_lat", "" + location.latitude)
+                    Log.d("new_long", "" + location.longitude)
+                    mPointsList?.add(mLatLng)
+                }
+            }
         }
     }
 
-    private fun getLastLocation() {
-        if (checkIfPermissionGranted()) {
+    private fun initListener() {
 
-            if (isLocationEnabled(mLocationManager)) {
-                Log.d("permission_result","location_enabled")
-            }
-
-        } else {
-            Log.d("permission_denied","please_grant_the_permission")
-            requsetPermission()
+        binding?.startButton?.setOnClickListener {
+            resetData()
+            getLocation()
+            binding?.stopButton?.visibility = View.VISIBLE
         }
+
+        binding?.stopButton?.setOnClickListener {
+            mIsCollectLocation = false
+            mMap?.apply {
+                this.clear()
+                this.addPolyline(
+                    PolylineOptions().addAll(mPointsList!!).width(13f).color(Color.BLUE)
+                        .geodesic(true)
+                )
+                this.moveCamera(CameraUpdateFactory.newLatLngZoom(mPointsList?.last()!!, 15f))
+            }
+            mFusedLocationProvider?.removeLocationUpdates(mLocationCallback)
+            binding?.stopButton?.visibility = View.GONE
+        }
+    }
+
+    private fun resetData() {
+        mIsCollectLocation = true
+        mMap.clear()
+        mPointsList?.clear()
+    }
+
+    private fun getLocation() {
+        if (checkIfPermissionGranted()) {
+            if (isLocationEnabled(mLocationManager)) {
+                Log.d("location_enabled", "location_enabled")
+                requestNewLocations()
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestNewLocations() {
+        mLocationRequest.setPriority(com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(50);
+        mLocationRequest.setSmallestDisplacement(10f)
+
+        //mLocationRequest.setNumUpdates(10);
+        mFusedLocationProvider.requestLocationUpdates(
+            mLocationRequest,
+            mLocationCallback,
+            Looper.myLooper()!!
+        )
+
+
     }
 
     private fun checkIfPermissionGranted(): Boolean {
@@ -145,14 +187,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        if(requestCode == PERMISSION_REQUEST_CODE_LOCATION){
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                Log.d("permission_result","coarse_location_permission_granted")
+        if (requestCode == PERMISSION_REQUEST_CODE_LOCATION) {
+
+            var mLocationPermissionGranted = false
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("permission_result", "coarse_location_permission_granted")
+                mLocationPermissionGranted = true
+            } else {
+                mLocationPermissionGranted = false
             }
 
-            if (grantResults.isNotEmpty() && grantResults[1] == PackageManager.PERMISSION_GRANTED){
-                Log.d("permission_result","fine_location_permission_granted")
-            }
+            getLocation()
+
         }
     }
 
@@ -169,17 +215,23 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mFusedLocationProvider?.lastLocation?.addOnSuccessListener {
+            if (it != null) {
+                val mCurrentLocation = LatLng(it.latitude, it.longitude)
+                mPointsList?.add(mCurrentLocation)
+                mMap?.addMarker(MarkerOptions().position(mCurrentLocation).title("you are here"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mCurrentLocation, 15f))
+            } else {
+                // Add a marker in Sydney and move the camera
+                val sydney = LatLng(25.20, 55.270)
+                mMap.addMarker(MarkerOptions().position(sydney).title("Marker in dubai"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13f))
+            }
+        }
 
-        mMap.addPolyline(
-            PolylineOptions().addAll(mPointsList!!).width(15f).color(Color.RED).geodesic(true)
-        )
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(25.20, 55.270)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13f))
     }
 
     override fun onLocationChanged(location: Location) {
@@ -187,7 +239,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
             val latLng: LatLng
             latLng = LatLng(location.latitude, location.longitude)
             mPointsList?.add(latLng)
+
+            Log.d("location_changed_lat", "" + location.latitude)
+            Log.d("location_changed_long", "" + location.longitude)
         }
     }
     //endregion
+
 }
