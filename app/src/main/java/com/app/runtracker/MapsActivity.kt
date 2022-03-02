3cbf46d46d75725c752fb3dcfc9b8e1385db538d
raheez66 <raheez66@gmail.com>
2022-03-02 11:19:06 +0400

    added location permissions

--- app/src/main/java/com/app/runtracker/MapsActivity.kt
+++ app/src/main/java/com/app/runtracker/MapsActivity.kt
@@ -1,11 +1,17 @@
 package com.app.runtracker
 
+import android.content.Context
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.location.Location
 import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.location.LocationManagerCompat.isLocationEnabled
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -14,19 +20,29 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.app.runtracker.databinding.ActivityMapsBinding
-import com.google.android.gms.maps.model.Polyline
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.PolylineOptions
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
+import java.util.jar.Manifest
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback,LocationListener {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
+
+    //region declartation
+    companion object {
+        final val PERMISSION_REQUEST_CODE_LOCATION = 1000
+    }
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
     private var mIsCollectLocation: Boolean = false
-    private var mPointsList : ArrayList<LatLng>?= null
+    private var mPointsList: ArrayList<LatLng>? = null
+
+    private lateinit var mFusedLocationProvider: FusedLocationProviderClient
+    private lateinit var mLocationManager: LocationManager
+    //endregion
+
+
+    //region lifecycle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -39,10 +55,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,LocationListener {
         mapFragment.getMapAsync(this)
 
         initListener()
-        val mPoint1 = LatLng(21.00,51.00)
-        val mPoint2 = LatLng(31.00,61.00)
-        val mPoint3 = LatLng(41.00,71.00)
-        val mPoint4 = LatLng(51.00,81.00)
+        val mPoint1 = LatLng(21.00, 51.00)
+        val mPoint2 = LatLng(31.00, 61.00)
+        val mPoint3 = LatLng(41.00, 71.00)
+        val mPoint4 = LatLng(51.00, 81.00)
 
         mPointsList = ArrayList<LatLng>()
         mPointsList?.add(mPoint1)
@@ -50,28 +66,100 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,LocationListener {
         mPointsList?.add(mPoint3)
         mPointsList?.add(mPoint4)
 
+
+        mFusedLocationProvider = LocationServices.getFusedLocationProviderClient(this)
+        mLocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        getLastLocation()
+
     }
 
-    private fun initListener() {
+    override fun onResume() {
+        super.onResume()
+    }
 
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    //endregion
+
+
+    //region methods
+    private fun initListener() {
         binding?.startButton?.setOnClickListener {
             mIsCollectLocation = true
-            Toast.makeText(this,"started",Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "started", Toast.LENGTH_SHORT).show()
         }
-
         binding?.stopButton?.setOnClickListener {
-
-
             mIsCollectLocation = false
             mMap.clear()
-            mMap.addPolyline(PolylineOptions().addAll(mPointsList!!).width(15f).color(Color.BLUE).geodesic(true))
-            Toast.makeText(this,"stoped",Toast.LENGTH_SHORT).show()
-
+            mMap.addPolyline(
+                PolylineOptions().addAll(mPointsList!!).width(15f).color(Color.BLUE).geodesic(true)
+            )
+            Toast.makeText(this, "stopped", Toast.LENGTH_SHORT).show()
         }
-
-
     }
 
+    private fun getLastLocation() {
+        if (checkIfPermissionGranted()) {
+
+            if (isLocationEnabled(mLocationManager)) {
+                Log.d("permission_result","location_enabled")
+            }
+
+        } else {
+            Log.d("permission_denied","please_grant_the_permission")
+            requsetPermission()
+        }
+    }
+
+    private fun checkIfPermissionGranted(): Boolean {
+
+        return ActivityCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+    }
+
+
+    private fun requsetPermission() {
+        val mList = arrayOf(
+            android.Manifest.permission.ACCESS_COARSE_LOCATION,
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        )
+        ActivityCompat.requestPermissions(this, mList, PERMISSION_REQUEST_CODE_LOCATION)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == PERMISSION_REQUEST_CODE_LOCATION){
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Log.d("permission_result","coarse_location_permission_granted")
+            }
+
+            if (grantResults.isNotEmpty() && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                Log.d("permission_result","fine_location_permission_granted")
+            }
+        }
+    }
+
+    //endregion
+
+
+    //region interfaces
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -84,19 +172,22 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,LocationListener {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        mMap.addPolyline(PolylineOptions().addAll(mPointsList!!).width(15f).color(Color.RED).geodesic(true))
+        mMap.addPolyline(
+            PolylineOptions().addAll(mPointsList!!).width(15f).color(Color.RED).geodesic(true)
+        )
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(25.20, 55.270)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,13f))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13f))
     }
 
     override fun onLocationChanged(location: Location) {
-        if (mIsCollectLocation){
-            val latLng : LatLng
-            latLng = LatLng(location.latitude,location.longitude)
+        if (mIsCollectLocation) {
+            val latLng: LatLng
+            latLng = LatLng(location.latitude, location.longitude)
             mPointsList?.add(latLng)
         }
     }
+    //endregion
 }
